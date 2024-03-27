351d73a57b7ced077c869831398d542230a17657
DilipWebPandit <dilip7065kumar@gmail.com>
2024-03-27 20:11:07 +0530

    This is w3school-clone

---
+++ index.js
@@ -0,0 +1,17 @@
+
+for(var num = 0; num <= 21; num ++){
+    document.querySelectorAll('.list_item_js')[num].style.display = 'none';
+}
+
+
+
+function fgreater(){
+    // let num1 = 0;
+    // num1 += 1;
+    document.querySelectorAll('.list_item_js')[0].style.display = 'none';
+    // var num2 = 18
+    // num2 += 1;
+    // document.querySelectorAll('.list_item_js')[num2].style.display = 'inline';
+    console.log("I am clicked");
+}
+
